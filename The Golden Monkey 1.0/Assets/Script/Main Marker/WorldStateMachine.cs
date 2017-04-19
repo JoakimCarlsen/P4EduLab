c9a052b1f0d8304e89d651b35a030e634671f14f
JoakimCarlsen <Joakim@h42.aau1x-2.klient.sydhavn.site.aau.dk>
2017-04-19 09:30:43 +0200

    Added project folder

---
+++ The Golden Monkey 1.0/Assets/Script/Main Marker/WorldStateMachine.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldStateMachine : MonoBehaviour {
+
+    public GameObject Player;
+    public GameObject[] positionObjects;
+    public bool[] finishQuest ;
+
+    public enum WorldStates{
+        Start,
+        Position1,
+        Position2,
+        Position3,
+        Position4,
+        Position5,
+        Finish
+    }
+
+    public WorldStates currentState = WorldStates.Start;
+
+	// Use this for initialization
+	void Start () {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        DontDestroyOnLoad(this.gameObject);
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (currentState)
+        {
+            case WorldStates.Start:
+                Player.transform.position = positionObjects[0].transform.position;
+                break;
+            case WorldStates.Position1:
+                Player.transform.position = positionObjects[1].transform.position;
+               
+                break;
+            case WorldStates.Position2:
+                Player.transform.position = positionObjects[2].transform.position;
+                break;
+            case WorldStates.Position3:
+                Player.transform.position = positionObjects[3].transform.position;
+                break;
+            case WorldStates.Position4:
+                Player.transform.position = positionObjects[4].transform.position;
+                break;
+            case WorldStates.Position5:
+                Player.transform.position = positionObjects[5].transform.position;
+                break;
+            case WorldStates.Finish:
+                break;
+        }
+            
+	}
+}
---
+++ The Golden Monkey 1.0/Assets/Script/Totem Challange/TotemAnsweringMethod.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TotemAnsweringMethod : MonoBehaviour {
+
+	public bool sign1 = false;
+	public bool sign2 = false;
+	public bool sign3 = false;
+	public bool sign4 = false;
+
+	public string guess;
+	public string answer = "4321";
+
+	public List<int> signOrder = new List<int>();
+
+
+	public GameObject[] buttons;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (sign1 == true)
+		{
+			signOrder.Add (1);
+			sign1 =false;
+			// Remove onTrigger for GameObject here so it cannot be pressed again
+		}
+
+		if (sign2 == true)
+		{
+			signOrder.Add (2);
+			sign2 =false;
+			// Remove onTrigger for GameObject here so it cannot be pressed again
+		}
+
+		if (sign3 == true)
+		{
+			signOrder.Add (3);
+			sign3 =false;
+			// Remove onTrigger for GameObject here so it cannot be pressed again
+		}
+
+		if (sign4 == true)
+		{
+			signOrder.Add (4);
+			sign4 =false;
+			// Remove onTrigger for GameObject here so it cannot be pressed again
+		}
+
+		if (signOrder.Count == 4){
+			guess = signOrder[0].ToString()+signOrder[1].ToString()+signOrder[2].ToString()+signOrder[3].ToString();
+
+			if (guess == answer)
+			{
+				print ("HEEU ERGETJHIAER");
+			} else if (guess != answer){
+				print ("WRONG MOFO");
+			}
+		}
+			
+
+	}
+}
