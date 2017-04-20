6a408243df8a879547500db422a7d452bd58968c
Mikkel <mikkel.thynov@gmail.com>
2017-04-20 10:33:56 +0200

    With the base for the 'Distract Monkey' minigame

---
+++ The Golden Monkey 1.0/Assets/Script/Distract Monkey/IsHit.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsHit : MonoBehaviour {
+
+	public GameObject[] fruits;
+//	------------------------------ apple
+//	public GameObject apple;
+//	------------------------------ banana
+//	public GameObject banana;
+//	------------------------------ pear
+//	public GameObject pear;
+
+	public GameObject cube;
+	public GameObject basket;
+
+	public GameObject ImageTarget2;
+
+	AudioSource AppleHitSoundFX;
+
+
+	bool isDragged = false;									// boolean to make sure that only one apple at a time can be moved
+
+	public bool [] fruitReady;
+	public bool [] fruitDone;
+
+//	bool apple1Ready = false;
+//	bool apple2Ready = false;
+//	bool apple3Ready = false;
+//	bool apple1Done = false;
+//	bool apple2Done = false;
+//	bool apple3Done = false;
+
+//	bool getPoint = false;											// Score system (NEED FIX)
+//	int scoreCount;
+
+
+	// Use this for initialization
+	void Start () {
+
+		for (int i = 0; i < fruitReady.Length; i++) {				// assign booleans in array with a false value
+			fruitReady [i] = false;
+		}
+			
+
+
+		cube = GameObject.Find ("Camera");
+
+		basket = GameObject.Find ("Basket");
+
+		ImageTarget2 = GameObject.Find ("ImageTarget2");
+
+		AppleHitSoundFX = GetComponent<AudioSource> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col) {
+		
+		for (int i = 0; i < fruits.Length; i++) {
+			if (col.gameObject.name == ("fruit" + i) && isDragged == false) { 		// && fruitDone[0] == false			forloob
+				print ("bananaIsHit");
+				fruits[i].transform.parent = cube.transform;
+
+				if (i <= 1) {
+					fruits[i].transform.localPosition = new Vector3(0.004f, -0.08f, 0.9f);
+				} else {
+					fruits[i].transform.localPosition = new Vector3(0.004f, -0.01f, 0.2f);
+					fruits[i].transform.localScale = new Vector3(2.2f, 2.2f, 2.2f);
+				}
+				isDragged = true;
+				//			fruitReady[0] = true;
+				//			AppleHitSoundFX.Play (); 		
+			}
+			print ("Loop: "+i);
+		}
+
+
+//		if (col.CompareTag("Apple2") && isDragged == false && apple2Done == false) {
+//			print ("apple2IsHit");
+//		
+//			apple2.transform.parent = cube.transform;
+//			apple2.transform.localPosition = new Vector3(0.004f, -0.05f, 0.1f); //(0.004f, -0.05f, 0.25f)
+//			apple2.transform.localScale = new Vector3 (2.2f, 2.2f, 2.2f);
+//			isDragged = true;
+//			apple2Ready = true;
+//			AppleHitSoundFX.Play ();
+//
+//
+//		}
+//
+//		if (col.CompareTag("Apple3") && isDragged == false && apple3Done == false) {
+//			print ("apple3IsHit");
+//
+//			apple3.transform.parent = cube.transform;
+//			apple3.transform.localPosition = new Vector3(0.004f, -0.05f, 0.1f);
+//			apple3.transform.localScale = new Vector3 (2.2f, 2.2f, 2.2f);
+//			isDragged = true;
+//			apple3Ready = true;
+//			AppleHitSoundFX.Play ();
+//
+//		}
+//
+		if (col.gameObject.name == ("StoneBowl")) {
+			print ("basketIsHit");
+			isDragged = false;
+	
+//			if (apple1Ready == true && apple1Done == false) {
+//				apple1.transform.parent = ImageTarget2.transform;
+//				apple1.transform.localPosition = new Vector3 (0.2f, 0.2f, 0.08f);
+//				apple1.transform.localScale = new Vector3 (2.6f, 2.6f, 2.6f);
+//				scoreCount++;
+//				apple1Done = true;
+//
+////				getPoint = true;
+//			}
+
+//			if (apple2Ready == true && apple2Done == false) {
+//				apple2.transform.parent = ImageTarget2.transform;
+//				apple2.transform.localPosition = new Vector3(-0.16f , 0.21f, -0.1f);
+//				apple2.transform.localScale = new Vector3 (2.6f, 2.6f, 2.6f);
+//				scoreCount++;
+//				apple2Done = true;
+//			}
+//
+//			if (apple3Ready == true && apple3Done == false) {
+//				apple3.transform.parent = ImageTarget2.transform;
+//				apple3.transform.localPosition = new Vector3(0.21f , 0.2f, -0.19f); //0.12 , .. , 0,1
+//				apple3.transform.localScale = new Vector3 (2.6f, 2.6f, 2.6f);
+//				scoreCount++;
+//				apple3Done = true;
+			}
+		}
+	}
+
+//	void OnGUI() {
+//		if (getPoint == true) {	
+//			GUIStyle style = new GUIStyle();
+//			style.fontSize = 80;
+//			GUI.color = Color.yellow;
+//			GUI.Label (new Rect (Screen.width/2-25, Screen.height/2-50, 100, 100), ""+scoreCount, style);
+//		}
+//	}
+//}
--- The Golden Monkey 1.0/Assets/Script/Totem Challange/TotemAnsweringMethod.cs
+++ The Golden Monkey 1.0/Assets/Script/Totem Challange/TotemAnsweringMethod.cs
@@ -11,16 +11,12 @@ public class TotemAnsweringMethod : MonoBehaviour {
 	public bool sign4 = false;
 
 	public string guess;
-
-	// Correct order of the correct answer
 	public string answer = "4321";
 
-	// A list of ints for the sign order
 	public List<int> signOrder = new List<int>();
 
-	// Array of the four buttons
-	public GameObject[] buttons;
 
+	public GameObject[] buttons;
 
 	// Use this for initialization
 	void Start () {
@@ -31,46 +27,40 @@ public class TotemAnsweringMethod : MonoBehaviour {
 	void Update () {
 		if (sign1 == true)
 		{
-			// Adds a 1 to the signOrder list
 			signOrder.Add (1);
-			// The bool to activate this function is turned false, so it only adds one number
 			sign1 =false;
-			// The trigger for the gameObject is turned false, so it cannot be pressed more than once
-			buttons[0].GetComponent<BoxCollider>().isTrigger= false;
+			// Remove onTrigger for GameObject here so it cannot be pressed again
 		}
 
 		if (sign2 == true)
 		{
 			signOrder.Add (2);
 			sign2 =false;
-			buttons[1].GetComponent<BoxCollider>().isTrigger= false;
+			// Remove onTrigger for GameObject here so it cannot be pressed again
 		}
 
 		if (sign3 == true)
 		{
 			signOrder.Add (3);
 			sign3 =false;
-			buttons[2].GetComponent<BoxCollider>().isTrigger= false;
+			// Remove onTrigger for GameObject here so it cannot be pressed again
 		}
 
 		if (sign4 == true)
 		{
 			signOrder.Add (4);
 			sign4 =false;
-			buttons[3].GetComponent<BoxCollider>().isTrigger= false;
+			// Remove onTrigger for GameObject here so it cannot be pressed again
 		}
 
-		// When the length of the signOrder list is 4 the following happens
 		if (signOrder.Count == 4){
-			// The list of the 4 numbers are turned into a string and saved in the guess variable
 			guess = signOrder[0].ToString()+signOrder[1].ToString()+signOrder[2].ToString()+signOrder[3].ToString();
 
-			// The guess and the answer variables are compared. If they are the same you win, if not you lose
 			if (guess == answer)
 			{
-				print ("Correct");
+				print ("HEEU ERGETJHIAER");
 			} else if (guess != answer){
-				print ("Wrong");
+				print ("WRONG MOFO");
 			}
 		}
 			
