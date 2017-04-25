8bd2114a2f15ec01f429af743170fbcb26128564
ipperxd <ipperxd@gmail.com>
2017-04-25 14:27:20 +0200

    Save tiger

--- The Golden Monkey 1.0/Assets/Script/Save Tiger/SaveTigerTrigger.cs
+++ The Golden Monkey 1.0/Assets/Script/Save Tiger/SaveTigerTrigger.cs
@@ -8,7 +8,8 @@ public class SaveTigerTrigger : MonoBehaviour {
     public GameObject keyObject;
     public GameObject keyHandle;
     public GameObject reward;
-    public GameObject locks; 
+    public GameObject locker;
+    public GameObject cage;
 
 	// Use this for initialization
 	void Start () {
@@ -27,27 +28,30 @@ public class SaveTigerTrigger : MonoBehaviour {
             keyObject = other.gameObject; 
             print("Pick up key");
             keyAttached = true;
-            keyObject.GetComponent<BoxCollider>().size = new Vector3(0, 0, 0); 
+            keyObject.GetComponent<BoxCollider>().enabled = false; 
             keyObject.transform.parent = keyHandle.transform;
             keyObject.transform.position = keyHandle.transform.position;
-            keyObject.transform.rotation = new Quaternion(0, 0, 0, 0);
-            keyObject.transform.localScale = new Vector3(50f, 50f, 7f);
+            keyObject.transform.localEulerAngles = new Vector3(0,75,0);
+            keyObject.transform.localScale = new Vector3(10f, 10f, 10f);
+            
         }
 
         if (keyAttached == true)
         {
-            if (keyObject.GetComponent<Key>().rightKey == false)
+            if (keyObject.GetComponent<Key>().rightKey == false && other.gameObject.tag == "Lock")
             {
                 print("Wrong Lock");
                
                 Destroy(keyObject);
 
             }
-            else if (keyObject.GetComponent<Key>().rightKey == true)
+            else if (keyObject.GetComponent<Key>().rightKey == true && other.gameObject.tag == "Lock")
             {
                 print("The Right Key");
                 reward.SetActive(true);
                 Destroy(keyObject);
+                Destroy(locker);
+                Destroy(cage);
             }
         }
         
