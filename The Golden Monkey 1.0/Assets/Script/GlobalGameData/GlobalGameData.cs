cd1b01d73a2d9a8483dcc79a08ab0b6c1a902491
JoakimCarlsen <Joakim@h338.aau1x-2.klient.sydhavn.site.aau.dk>
2017-04-27 14:32:11 +0200

    Fixed a minor bug

--- The Golden Monkey 1.0/Assets/Script/GlobalGameData/GlobalGameData.cs
+++ The Golden Monkey 1.0/Assets/Script/GlobalGameData/GlobalGameData.cs
@@ -7,149 +7,134 @@ using UnityEngine.SceneManagement;
 public class GlobalGameData : MonoBehaviour
 {
 
-    public Dropdown pickStarterScene;
-    public Button startButton;
-    public GameObject globalDataObject;
-    public int startValue;
-    public int currentPiecesOfTreasure = 0;
-    public int maxPiecesOfTreasure = 5;
+	public Dropdown pickStarterScene;
+	public Button startButton;
+	public GameObject globalDataObject;
+	public int startValue;
+	public int currentPiecesOfTreasure = 0;
+	public int maxPiecesOfTreasure = 5;
 
 
-    // Use this for initialization
-    void Start()
-    {
-        DontDestroyOnLoad(globalDataObject);
-    }
+	// Use this for initialization
+	void Start ()
+	{
+		DontDestroyOnLoad (globalDataObject);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update ()
+	{
         
-    }
+	}
 
 
-    public void PickStartScene()
-    {
-        if (pickStarterScene.GetComponent<Dropdown>().value == 0)
-        {
-            startValue = 1;   
-            SceneManager.LoadScene(1);
-        }
-        else if (pickStarterScene.GetComponent<Dropdown>().value == 1)
-        {
-            startValue = 2;
-            SceneManager.LoadScene(2);
-        }
-        else if (pickStarterScene.GetComponent<Dropdown>().value == 2)
-        {
-            startValue = 3;
-            SceneManager.LoadScene(3);
+	public void PickStartScene ()
+	{
+		if (pickStarterScene.GetComponent<Dropdown> ().value == 0) {
+			startValue = 1;   
+			SceneManager.LoadScene (1);
+		} else if (pickStarterScene.GetComponent<Dropdown> ().value == 1) {
+			startValue = 2;
+			SceneManager.LoadScene (2);
+		} else if (pickStarterScene.GetComponent<Dropdown> ().value == 2) {
+			startValue = 3;
+			SceneManager.LoadScene (3);
 
-        }
-        else if (pickStarterScene.GetComponent<Dropdown>().value == 3)
-        {
-            startValue = 4;
-            SceneManager.LoadScene(4);
-        }
-        else if (pickStarterScene.GetComponent<Dropdown>().value == 4)
-        {
-            startValue = 5;
-            SceneManager.LoadScene(5);
-        }
-    }
+		} else if (pickStarterScene.GetComponent<Dropdown> ().value == 3) {
+			startValue = 4;
+			SceneManager.LoadScene (4);
+		} else if (pickStarterScene.GetComponent<Dropdown> ().value == 4) {
+			startValue = 5;
+			SceneManager.LoadScene (5);
+		}
+	}
 
-    public void ChangeScenes()
-    {
-        switch (startValue)
-        {
-            case 1:
-                switch (currentPiecesOfTreasure)
-                {
+	public void ChangeScenes ()
+	{
+		switch (startValue) {
+		case 1:
+			switch (currentPiecesOfTreasure) {
                    
-                    case 1:
-                        SceneManager.LoadScene(2);
-                        break;
-                    case 2:
-                        SceneManager.LoadScene(3);
-                        break;
-                    case 3:
-                        SceneManager.LoadScene(4);
-                        break;
-                    case 4:
-                        SceneManager.LoadScene(5);
-                        break;
-                }
-                break;
-            case 2:
-                switch (currentPiecesOfTreasure)
-                {
-                    case 1:
-                        SceneManager.LoadScene(3);
-                        break;
-                    case 2:
-                        SceneManager.LoadScene(4);
-                        break;
-                    case 3:
-                        SceneManager.LoadScene(5);
-                        break;
-                    case 4:
-                        SceneManager.LoadScene(1);
-                        break;
-                }
+			case 1:
+				SceneManager.LoadScene (2);
+				break;
+			case 2:
+				SceneManager.LoadScene (3);
+				break;
+			case 3:
+				SceneManager.LoadScene (4);
+				break;
+			case 4:
+				SceneManager.LoadScene (5);
+				break;
+			}
+			break;
+		case 2:
+			switch (currentPiecesOfTreasure) {
+			case 1:
+				SceneManager.LoadScene (3);
+				break;
+			case 2:
+				SceneManager.LoadScene (4);
+				break;
+			case 3:
+				SceneManager.LoadScene (5);
+				break;
+			case 4:
+				SceneManager.LoadScene (1);
+				break;
+			}
 
-                break;
-            case 3:
-                switch (startValue)
-                {
-                    case 1:
-                        SceneManager.LoadScene(4);
-                        break;
-                    case 2:
-                        SceneManager.LoadScene(5);
-                        break;
-                    case 3:
-                        SceneManager.LoadScene(1);
-                        break;
-                    case 4:
-                        SceneManager.LoadScene(2);
-                        break;
-                }
-                break;
-            case 4:
-                switch (startValue)
-                {
-                    case 1:
-                        SceneManager.LoadScene(5);
-                        break;
-                    case 2:
-                        SceneManager.LoadScene(1);
-                        break;
-                    case 3:
-                        SceneManager.LoadScene(2);
-                        break;
-                    case 4:
-                        SceneManager.LoadScene(3);
-                        break;
-                }
-                break;
-            case 5:
-                switch (startValue)
-                {
-                    case 1:
-                        SceneManager.LoadScene(1);
-                        break;
-                    case 2:
-                        SceneManager.LoadScene(2);
-                        break;
-                    case 3:
-                        SceneManager.LoadScene(3);
-                        break;
-                    case 4:
-                        SceneManager.LoadScene(4);
-                        break;
-                }
-                break;
-        }
+			break;
+		case 3:
+			switch (currentPiecesOfTreasure) {
+			case 1:
+				SceneManager.LoadScene (4);
+				break;
+			case 2:
+				SceneManager.LoadScene (5);
+				break;
+			case 3:
+				SceneManager.LoadScene (1);
+				break;
+			case 4:
+				SceneManager.LoadScene (2);
+				break;
+			}
+			break;
+		case 4:
+			switch (currentPiecesOfTreasure) {
+			case 1:
+				SceneManager.LoadScene (5);
+				break;
+			case 2:
+				SceneManager.LoadScene (1);
+				break;
+			case 3:
+				SceneManager.LoadScene (2);
+				break;
+			case 4:
+				SceneManager.LoadScene (3);
+				break;
+			}
+			break;
+		case 5:
+			switch (currentPiecesOfTreasure) {
+			case 1:
+				SceneManager.LoadScene (1);
+				break;
+			case 2:
+				SceneManager.LoadScene (2);
+				break;
+			case 3:
+				SceneManager.LoadScene (3);
+				break;
+			case 4:
+				SceneManager.LoadScene (4);
+				break;
+			}
+			break;
+		}
 
-    }
+	}
 }
